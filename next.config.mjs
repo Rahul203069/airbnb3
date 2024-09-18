485f84851cfef2108c2c7c7c5b1bc001235d123b
Rahul203069 <157735088+Rahul203069@users.noreply.github.com>
2024-09-18 19:52:45 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {odule.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },};
 
 export default nextConfig;
