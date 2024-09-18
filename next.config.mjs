5770189de67aba0f0d2590faa66ff0b40260f83e
Rahul Sharma <rs3296472t@gmail.com>
2024-09-18 22:23:46 +0530

    Describe your changes here

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    eslint: {
+      // Disable ESLint during builds
+      ignoreDuringBuilds: true,
+    },
+  };
 export default nextConfig;
